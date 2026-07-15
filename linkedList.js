bb3b5e10d1be35693a5f84c94cb45f86f2797e15
Tyler McMillan <36271467+mcmityler@users.noreply.github.com>
2026-07-15 18:29:28 -0400

    Add test from bottom of Odin Project

--- linkedList.js
+++ linkedList.js
@@ -177,3 +177,14 @@ console.log(linkList.toString());
 console.log(linkList.removeAt(5));
 
 console.log(linkList.toString());
+
+const list = new LinkedList();
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+
+console.log(list.toString());
